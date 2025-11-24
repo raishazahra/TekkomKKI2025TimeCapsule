b425a77daa379bd17a81cc13ab22840344deff5e
Carlsson Khovis <carlkho99@gmail.com>
2025-11-24 18:05:53 +0700

    Initial commit.

---
+++ messages.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Oscar's message function.
+// Message format: NPM | Full Name | Message
+void oscar() {
+    puts("0006347853 | Oscar Oscar | Just you wait");
+}
+
+int main() {
+    oscar();
+
+    // Call your function after this.
+
+
+
+    return 0;
+}
